fded4bd8a8cbb00c19c88ba7923a8ad15e64d9ca
pooranijayakumar <36123843+pooranijayakumar@users.noreply.github.com>
2018-02-10 18:31:22 +0530

    Create oddeven.py

---
+++ beginner/oddeven.py
@@ -0,0 +1,8 @@
+a=float(input("enter the number:"))
+if(0<=a<=100000):
+ if (a%2==0):
+        print("the number is even")
+ else:
+    print("tne number is odd")
+else:
+    print("number is not valid")
