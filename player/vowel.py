4d3708b59677f5e7ec3a3a944c85c2fb80149de5
pooranijayakumar <36123843+pooranijayakumar@users.noreply.github.com>
2018-02-16 18:59:02 +0530

    Create vowel.py

---
+++ player/vowel.py
@@ -0,0 +1,13 @@
+letter=str(input("enter the letter:"))
+if(letter=="a"):
+  print("vowel")
+elif(letter=="e"):
+      print("vowel")
+elif(letter=="i"):
+          print("vowel")
+elif(letter=="o"):
+              print("vowel")
+elif(letter=="u"):
+              print("vowel")
+else:
+  print("consonent")
